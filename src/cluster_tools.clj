08d7ef3ab583522a3883e50f7142e9ca7cc8f636
tuh8888 <harrison.pielke-lombardo@ucdenver.edu>
2019-06-07 05:30:52 -0600

    More efficient map-reduce method

--- src/cluster_tools.clj
+++ src/cluster_tools.clj
@@ -1,14 +1,5 @@
-(ns cluster-tools)
-
-(defn nearest-cluster
-  [sample clusters best {:keys [cluster-match-fn]}]
-  (first
-    (reduce
-      (fn [{:keys [cluster score] :as best}  new-cluster]
-        (if-let [score (cluster-match-fn cluster sample score)]
-          {:score score :cluster new-cluster}
-          best))
-      best clusters)))
+(ns cluster-tools
+  (:require [com.climate.claypoole :as cp]))
 
 (defn update-cluster
   [clusters cluster sample merge-fn]
@@ -21,17 +12,25 @@
   (conj clusters (merge-fn nil sample)))
 
 (defn nearest-sample-cluster-pair
-  [samples clusters params]
-  (reduce
-    (fn [best sample]
-      (nearest-cluster sample clusters best params))
-    {} samples))
+  [samples clusters {:keys [cluster-sim-fn cluster-thresh]}]
+  (->> (cp/upfor (cp/ncpus) [sample samples
+                             cluster clusters]
+                 (print ".")
+                 {:score (cluster-sim-fn sample cluster) :cluster cluster :sample sample})
+       (reduce
+         (fn [best new]
+           (if (< (:score best) (:score new))
+             new
+             best))
+         {:score cluster-thresh})
+       (do (println))))
 
 (defn single-pass-cluster
   "Occurs in O(N^2*M) time"
   [samples clusters {:keys [cluster-merge-fn] :as params}]
-  (loop [clusters clusters
-         samples samples]
-    (if-let [best (nearest-sample-cluster-pair samples clusters params)]
-      (recur (disj samples (:sample best)) (update-cluster clusters (:cluster best) (:sample best) cluster-merge-fn))
-      (recur (disj samples (first samples)) (conj-cluster clusters (first samples) cluster-merge-fn)))))
+  (loop [samples samples
+         clusters clusters]
+    (let [best (nearest-sample-cluster-pair samples clusters params)]
+      (cond (:cluster best) (recur (disj samples (:sample best)) (update-cluster clusters (:cluster best) (:sample best) cluster-merge-fn))
+            (seq samples) (recur (set (rest samples)) (conj-cluster clusters (first samples) cluster-merge-fn))
+            :else clusters))))
