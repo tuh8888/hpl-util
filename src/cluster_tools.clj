7b5266fd00a7110791812564ff946cabbafa9a7c
tuh8888 <harrison.pielke-lombardo@ucdenver.edu>
2019-06-07 03:20:20 -0600

    Find best cluster-sample pair

--- src/cluster_tools.clj
+++ src/cluster_tools.clj
@@ -1,15 +1,14 @@
 (ns cluster-tools)
 
-(defn nearest
-  [clusters sample & [{:keys [cluster-match-fn]}]]
+(defn nearest-cluster
+  [sample clusters best {:keys [cluster-match-fn]}]
   (first
     (reduce
-      (fn [[best-cluster best-score] cluster]
-        (if-let [score (cluster-match-fn cluster sample best-score)]
-          [cluster score]
-          [best-cluster best-score]))
-      [nil nil]
-      clusters)))
+      (fn [{:keys [cluster score] :as best}  new-cluster]
+        (if-let [score (cluster-match-fn cluster sample score)]
+          {:score score :cluster new-cluster}
+          best))
+      best clusters)))
 
 (defn update-cluster
   [clusters cluster sample merge-fn]
@@ -17,16 +16,22 @@
         cluster (merge-fn cluster sample)]
     (conj clusters cluster)))
 
-(defn assoc-cluster
+(defn conj-cluster
   [clusters sample merge-fn]
   (conj clusters (merge-fn nil sample)))
 
-(defn single-pass-cluster
-  [samples clusters & [{:keys [cluster-merge-fn] :as params}]]
+(defn nearest-sample-cluster-pair
+  [samples clusters params]
   (reduce
-    (fn [clusters sample]
-      (if-let [cluster (nearest clusters sample params)]
-        (update-cluster clusters cluster sample cluster-merge-fn)
-        (assoc-cluster clusters sample cluster-merge-fn)))
-    clusters
-    samples))
+    (fn [best sample]
+      (nearest-cluster sample clusters best params))
+    {} samples))
+
+(defn single-pass-cluster
+  "Occurs in O(N^2*M) time"
+  [samples clusters {:keys [cluster-merge-fn] :as params}]
+  (loop [clusters clusters
+         samples samples]
+    (if-let [best (nearest-sample-cluster-pair samples clusters params)]
+      (recur (disj samples (:sample best)) (update-cluster clusters (:cluster best) (:sample best) cluster-merge-fn))
+      (recur (disj samples (first samples)) (conj-cluster clusters (first samples) cluster-merge-fn)))))
