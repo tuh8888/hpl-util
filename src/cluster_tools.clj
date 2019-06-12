6982794c5919a5f2248ec87e3f72f08d21518e4e
tuh8888 <harrison.pielke-lombardo@ucdenver.edu>
2019-06-12 05:58:21 -0600

    Use vectors to track groups

--- src/cluster_tools.clj
+++ src/cluster_tools.clj
@@ -1,9 +1,14 @@
 (ns cluster-tools
   (:require [linear-algebra :as linear-algebra]))
 
+(defn vec-remove
+  "remove elem in coll"
+  [coll pos]
+  (vec (concat (subvec coll 0 pos) (subvec coll (inc pos)))))
+
 (defn update-cluster
-  [clusters cluster sample merge-fn]
-  (let [clusters (disj clusters cluster)
+  [clusters cluster pos sample merge-fn]
+  (let [clusters (vec-remove clusters pos)
         cluster (merge-fn cluster sample)]
     (conj clusters cluster)))
 
@@ -20,15 +25,30 @@
 
 (defn single-pass-cluster
   "Occurs in O(N^2*M) time"
-  [{:keys [cluster-merge-fn] :as params} samples clusters]
-  (loop [samples samples
-         clusters clusters]
-    (let [{:keys [sample match]} (nearest-sample-cluster-pair samples clusters params)]
-      (cond match (let [samples (disj samples sample)
-                        clusters (update-cluster clusters match sample cluster-merge-fn)]
-                    (recur samples clusters))
-            (seq samples) (let [sample (first samples)
-                                samples (disj samples sample)
-                                clusters (conj-cluster clusters sample cluster-merge-fn)]
-                            (recur samples clusters))
-            :else clusters))))
+  [{:keys [cluster-merge-fn vector-fn] :as params} samples clusters]
+  (let [clusters (vec clusters)
+        samples (vec samples)]
+
+    (loop [samples (vec samples)
+           sample-vectors (->> samples
+                               (map vector-fn)
+                               (map #(linear-algebra/unit-vec params %))
+                               (vec))
+           clusters (vec clusters)]
+      (let [cluster-vectors (->> clusters
+                                 (map vector-fn)
+                                 (map #(linear-algebra/unit-vec params %)))
+            {:keys [i j]} (nearest-sample-cluster-pair sample-vectors cluster-vectors params)]
+
+        (let [sample (get samples i)
+              cluster (get clusters j)]
+          (cond cluster (let [samples (vec-remove samples i)
+                              sample-vectors (vec-remove sample-vectors i)
+                              clusters (update-cluster clusters cluster j sample cluster-merge-fn)]
+                          (recur samples sample-vectors clusters))
+                (seq samples) (let [sample (first samples)
+                                    samples (vec-remove samples 0)
+                                    sample-vectors (vec-remove sample-vectors 0)
+                                    clusters (conj-cluster clusters sample cluster-merge-fn)]
+                                (recur samples sample-vectors clusters))
+                :else clusters))))))
--- src/linear_algebra.clj
+++ src/linear_algebra.clj
@@ -1,88 +1,71 @@
 (ns linear-algebra
   (:require [uncomplicate.commons.core :as uncomplicate]
-            [uncomplicate.neanderthal.core :as thal]
-            [uncomplicate.neanderthal.real :as thal-real]))
+            [uncomplicate.neanderthal.core :as thal]))
 
 (defn unit-vec
-  [factory v]
-  (uncomplicate/let-release [v (thal/vctr factory v)
-                             alpha-1 (thal/nrm2 v)
-                             alpha (/ alpha-1)
-                             result (thal/scal alpha v)]
-    (seq result)))
+  [{:keys [factory]} v]
+  (uncomplicate/with-release [v (thal/vctr factory v)
+                              alpha-1 (thal/nrm2 v)
+                              alpha (/ alpha-1)
+                              result (thal/scal alpha v)]
+    (vec (seq result))))
 
-(defn cosine-sim
-  [factory v1 v2]
-  (uncomplicate/let-release [v1 (unit-vec factory v1)
-                             v2 (unit-vec factory v2)
-                             result (thal/dot v1 v2)]
-    result))
-
-(defn unit-vec-sum
-  [factory & vectors]
+(defn vec-sum
+  [& vectors]
   (let [vectors (keep seq vectors)]
-    (if (seq vectors)
+    (when (seq vectors)
       (->> vectors
-           (reduce (fn [v1 v2]
-                     (if v1
-                       (uncomplicate/let-release [v1 (thal/vctr factory v1)
-                                                  v2 (thal/vctr factory v2)
-                                                  sum-v (thal/xpy v1 v2)]
-                         (seq sum-v))
-                       v2))
-                   nil)
-           (unit-vec factory)))))
+           (apply map +)))))
 
 
 (defn vectors->matrix
   [{:keys [factory]} vectors]
-  (let [d (some #(count %) vectors)]
+  (let [m (count (first vectors))
+        n (count vectors)]
     (->> vectors
-         (mapcat seq)
-         (thal/ge factory d (count vectors)))))
+         (map seq)
+         (flatten)
+         (thal/ge factory m n))))
 
 (defn mdot
-  [{:keys [vector-fn] :as params} s1 s2]
-  (let [v1 (map vector-fn s1)
-        v2 (map vector-fn s2)]
-    (uncomplicate/let-release [s1-mat (vectors->matrix params v1)
-                               s1-mat-trans (thal/trans s1-mat)
-                               s2-mat (vectors->matrix params v2)]
-      #_(log/info #_(thal/mrows s1) #_(thal/ncols s1) #_(thal/mrows s2) #_(thal/ncols s2))
-      (thal/mm s1-mat-trans s2-mat))))
-
-
-(defn best-in-row-from-matrix
-  [score-mat init i s]
-  (->> s
-       (map-indexed vector)
-       (reduce
-         (fn [{:keys [score] :as best} [j s]]
-           (uncomplicate/let-release [new-score (thal-real/entry score-mat i j)]
-             (if (< score new-score)
-               {:match s :score (float new-score)}
-               best)))
-         init)))
-
-(defn find-best-match
   [params s1 s2]
-  (uncomplicate/let-release [score-mat (mdot params s1 s2)]
-    (when score-mat
-      (->> s1
-           (map-indexed vector)
-           (reduce
-             (fn [best [i sample]]
-               (-> (best-in-row-from-matrix score-mat best i s2)
-                   (assoc :sample sample)))
-             {:score 0})))))
+  (uncomplicate/with-release [s1-mat (vectors->matrix params s1)
+                              s1-mat-trans (thal/trans s1-mat)
+                              s2-mat (vectors->matrix params s2)
+                              result (thal/mm s1-mat-trans s2-mat)]
+    #_(log/info #_(thal/mrows s1) #_(thal/ncols s1) #_(thal/mrows s2) #_(thal/ncols s2))
+    (vec (doall (map vec result)))))
+
+(defn find-best-match-in-row
+  [row]
+  (->> row
+       (map-indexed (fn [j score] {:score score :i j}))
+       (reduce (fn [best new]
+                 (if (< (:score best) (:score new))
+                   new
+                   best))
+               {:score 0})))
+
 
 (defn find-best-row-matches
   [params s1 s2]
-  (uncomplicate/let-release [score-mat (mdot params s1 s2)]
+  (uncomplicate/with-release [score-mat (mdot params s1 s2)]
     (when score-mat
-      (->> s1
-           (map-indexed (fn [i sample]
-                          (-> score-mat
-                              (best-in-row-from-matrix {:score 0} i s2)
-                              (assoc :sample sample))))
+      (->> score-mat
+           (map-indexed (fn [i row]
+                          (-> row
+                              (find-best-match-in-row)
+                              (assoc :j i))))
            (doall)))))
+
+
+(defn find-best-match
+  [params s1 s2]
+  (->> (find-best-row-matches params s1 s2)
+       (reduce (fn [best new]
+                 (if (< (:score best) (:score new))
+                   new
+                   best))
+               {:score 0})
+       (doall)))
+
