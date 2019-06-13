1de3c8a6a5dd7366d9f98ba2a69d20fa86512f2c
tuh8888 <harrison.pielke-lombardo@ucdenver.edu>
2019-06-13 03:16:50 -0600

    Fixed clustering param order

--- src/cluster_tools.clj
+++ src/cluster_tools.clj
@@ -7,17 +7,17 @@
   (vec (concat (subvec coll 0 pos) (subvec coll (inc pos)))))
 
 (defn update-cluster
-  [clusters cluster pos sample {:keys [cluster-merge-fn] :as params}]
+  [{:keys [cluster-merge-fn] :as params} clusters cluster pos sample]
   (let [clusters (vec-remove clusters pos)
-        cluster (cluster-merge-fn cluster sample params)]
+        cluster (cluster-merge-fn params cluster sample)]
     (conj clusters cluster)))
 
 (defn conj-cluster
-  [clusters sample {:keys [cluster-merge-fn] :as params}]
-  (conj clusters (cluster-merge-fn nil sample params)))
+  [{:keys [cluster-merge-fn] :as params} clusters sample]
+  (conj clusters (cluster-merge-fn params nil sample)))
 
 (defn nearest-sample-cluster-pair
-  [clusters samples {:keys [cluster-thresh] :as params}]
+  [{:keys [cluster-thresh] :as params} clusters samples]
   (when (and (seq samples) (seq clusters))
     (let [best (linear-algebra/find-best-match params samples clusters)]
       (when (< cluster-thresh (:score best))
@@ -37,16 +37,16 @@
       (let [cluster-vectors (->> clusters
                                  (map vector-fn)
                                  (pmap #(linear-algebra/unit-vec params %)))
-            {:keys [i j]} (nearest-sample-cluster-pair cluster-vectors sample-vectors params)]
+            {:keys [i j]} (nearest-sample-cluster-pair params cluster-vectors sample-vectors)]
         (let [sample (get samples i)
               cluster (get clusters j)]
           (cond cluster (let [samples (vec-remove samples i)
                               sample-vectors (vec-remove sample-vectors i)
-                              clusters (update-cluster clusters cluster j sample params)]
+                              clusters (update-cluster params clusters cluster j sample)]
                           (recur samples sample-vectors clusters))
                 (seq samples) (let [sample (first samples)
                                     samples (vec-remove samples 0)
                                     sample-vectors (vec-remove sample-vectors 0)
-                                    clusters (conj-cluster clusters sample params)]
+                                    clusters (conj-cluster params clusters sample)]
                                 (recur samples sample-vectors clusters))
                 :else clusters))))))
