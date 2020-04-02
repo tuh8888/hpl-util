07da66c16774e2165293adef8560df210d961845
Harrison <harrison.pielke-lombardo@cuanschutz.edu>
2020-04-02 15:11:23 -0600

    Clean up

--- src/python/jupyter/import_ipynb.py
+++ src/python/jupyter/import_ipynb.py
@@ -13,7 +13,7 @@ from nbformat import read
 
 def find_notebook(fullname, path=None):
     """find a notebook, given its fully qualified name and an optional path
-    
+
     This turns "foo.bar" into "foo/bar.ipynb"
     and tries turning "Foo_Bar" into "Foo Bar" if Foo_Bar
     does not exist.
